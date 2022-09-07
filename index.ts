f4283c4200ed6ac7c9b338fe8c73282d4d46b43d
Joe Diragi <joyod3@gmail.com>
2022-09-06 22:27:13 -0400

    Can't write to file on Deno?

--- index.ts
+++ index.ts
@@ -2,12 +2,13 @@
 import { serve } from "https://deno.land/std@0.154.0/http/server.ts";
 
 const port = 1337
-let status = false;
+let status = false
+let lastMsg = "No message yet):"
 
 async function reqHandler(req: Request) {
     if (req.url.endsWith("/builder")) {
         const json = await req.json()
-        writeJson(json)
+        lastMsg = JSON.stringify(json)
         const completionStatus = json.ciBuildRun.attributes["completionStatus"]
         if (completionStatus == "SUCCEEDED") {
             // Build success, publish passing badge
@@ -35,25 +36,9 @@ async function reqHandler(req: Request) {
             }
         })
     } else if (req.url.endsWith("/out")) {
-        let file
-        try {
-            file = await Deno.open("./out.json", { read: true })
-        } catch {
-            return new Response("No logs available yet.", { status: 404 })
-        }
-        return new Response(file?.readable)
+        return new Response(lastMsg)
     }
     return new Response(null, { status: 404 })
-}
-
-function writeJson(json: JSON): boolean {
-    try {
-        Deno.writeTextFileSync("./out.json", JSON.stringify(json))
-        return true
-    } catch (e) {
-        console.log(e)
-        return false
-    }
 }
 
 serve(reqHandler, { port: port })
