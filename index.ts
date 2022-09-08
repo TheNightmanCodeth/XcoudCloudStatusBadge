5ae9be92140fbddd8e849f4a852e7045fc956593
Joe Diragi <joyod3@gmail.com>
2022-09-08 14:44:19 -0400

    Try adding expires to combat GH cache

--- index.ts
+++ index.ts
@@ -17,11 +17,16 @@ async function reqHandler(req: Request) {
         const badgeColor = (testsFailed == 0) ? "green" : "red"
         const badgeUrl = `https://img.shields.io/badge/Tests-${testsFailed}%20Failing-${badgeColor}`
         const badgeSvg = await fetch(badgeUrl)
+        const etagEntry = await etag.calculate(`teststatus${testsFailed}`)
+        const expires = new Date()
+        expires.setHours(expires.getHours() + 1)
         return new Response(badgeSvg.body, { headers:
             {
-                'ETag': await etag.calculate(Date()),
+                'Expires': expires.toString(),
+                'ETag': etagEntry,
                 'Cache-Control': 'private, max-age=0, no-cache',
                 'Content-Type': 'image/svg+xml;charset=utf-8',
+                'Link': badgeUrl
             }
         })
     } else if (req.url.endsWith("/badges/build-status")) {
@@ -29,11 +34,16 @@ async function reqHandler(req: Request) {
         const badgeStatus = (buildStatus) ? "Passing" : "Failing"
         const badgeUrl = `https://img.shields.io/badge/Build-${badgeStatus}-${badgeColor}`
         const badgeSvg = await fetch(badgeUrl)
+        const etagEntry = await etag.calculate(`buildstatus${badgeStatus}`)
+        const expires = new Date()
+        expires.setHours(expires.getHours() + 1)
         return new Response(badgeSvg.body, { headers: 
             {
-                'ETag': await etag.calculate(Date()),
+                'Expires': expires.toString(),
+                'ETag': etagEntry,
                 'Cache-Control': 'private, max-age=0, no-cache',
                 'Content-Type': 'image/svg+xml;charset=utf-8',
+                'Link': badgeUrl
             }
         })
     }
